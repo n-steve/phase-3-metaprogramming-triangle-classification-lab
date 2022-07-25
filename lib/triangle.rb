fcbf743f5518cf018ba92c7249a77db45fbd090e
steve <steve.nguyen2013@gmail.com>
2022-07-25 13:48:19 -0700

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+ 
+attr_accessor :x,:y,:z
+
+
+def initialize(x, y, z)
+  @x = x
+  @y = y
+  @z = z
+  raise TriangleError unless [x,y,z].all?(&:positive?) && x + y > z && x + z > y && z + y > x
+end
+
+
+def kind
+  # check_triangle
+if x == y && y == z
+  :equilateral
+elsif x == y || y == z || x == z
+  :isosceles
+elsif x != y || y != z
+  :scalene
+  end
+end
+
+# def check_triangle
+#   raise TriangleError unless [x,y,z].all?(&:positive?) && x + y > z && x + z > y && z + y > x
+# end
+
+class TriangleError < StandardError
+
+end
+
+     
+# if x || y || z == 0 raise error
+# if x, y, z is negative raise error
+# if x + y < z etc.. raise error
+
+
+
+
 end
